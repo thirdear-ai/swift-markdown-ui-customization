af9fc452def083b30fa5b111ed4b1e551b7506b9
盒子里的猫 <hellohezhili@gmail.com>
2024-12-23 18:13:29 +0800

    Update

--- Sources/MarkdownUI/Views/Blocks/BlockNode+View.swift
+++ Sources/MarkdownUI/Views/Blocks/BlockNode+View.swift
@@ -1,33 +1,35 @@
 import SwiftUI
 
-// extension BlockNode: View {
-//     var body: some View {
-//         switch self {
-//         case .blockquote(let children):
-//             BlockquoteView(children: children)
-//         case .bulletedList(let isTight, let items):
-//             BulletedListView(isTight: isTight, items: items)
-//         case .numberedList(let isTight, let start, let items):
-//             NumberedListView(isTight: isTight, start: start, items: items)
-//         case .taskList(let isTight, let items):
-//             TaskListView(isTight: isTight, items: items)
-//         case .codeBlock(let fenceInfo, let content):
-//             CodeBlockView(fenceInfo: fenceInfo, content: content)
-//         case .htmlBlock(let content):
-//             ParagraphView(content: content)
-//         case .paragraph(let content):
-//             ParagraphView(content: content)
-//         case .heading(let level, let content):
-//             HeadingView(level: level, content: content)
-//         case .table(let columnAlignments, let rows):
-//             if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
-//                 TableView(columnAlignments: columnAlignments, rows: rows)
-//             }
-//         case .thematicBreak:
-//             ThematicBreakView()
-//         }
-//     }
-// }
+ extension BlockNode: View {
+     var body: some View {
+         Group {
+             switch self {
+             case .blockquote(let children):
+                 BlockquoteView(children: children)
+             case .bulletedList(let isTight, let items):
+                 BulletedListView(isTight: isTight, items: items)
+             case .numberedList(let isTight, let start, let items):
+                 NumberedListView(isTight: isTight, start: start, items: items)
+             case .taskList(let isTight, let items):
+                 TaskListView(isTight: isTight, items: items)
+             case .codeBlock(let fenceInfo, let content):
+                 CodeBlockView(fenceInfo: fenceInfo, content: content)
+             case .htmlBlock(let content):
+                 ParagraphView(content: content)
+             case .paragraph(let content):
+                 ParagraphView(content: content)
+             case .heading(let level, let content):
+                 HeadingView(level: level, content: content)
+             case .table(let columnAlignments, let rows):
+                 if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
+                     TableView(columnAlignments: columnAlignments, rows: rows)
+                 }
+             case .thematicBreak:
+                 ThematicBreakView()
+             }
+         }
+     }
+ }
 
 // MARK: - BlockListView
 struct BlockListView: View {
@@ -99,32 +101,7 @@ struct BlockNodeView: View {
     let model: BlockNodeModel
     
     var body: some View {
-        Group {
-            switch self.model.node {
-            case .blockquote(let children):
-                BlockquoteView(children: children)
-            case .bulletedList(let isTight, let items):
-                BulletedListView(isTight: isTight, items: items)
-            case .numberedList(let isTight, let start, let items):
-                NumberedListView(isTight: isTight, start: start, items: items)
-            case .taskList(let isTight, let items):
-                TaskListView(isTight: isTight, items: items)
-            case .codeBlock(let fenceInfo, let content):
-                CodeBlockView(fenceInfo: fenceInfo, content: content)
-            case .htmlBlock(let content):
-                ParagraphView(content: content)
-            case .paragraph(let content):
-                ParagraphView(content: content)
-            case .heading(let level, let content):
-                HeadingView(level: level, content: content)
-            case .table(let columnAlignments, let rows):
-                if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
-                    TableView(columnAlignments: columnAlignments, rows: rows)
-                }
-            case .thematicBreak:
-                ThematicBreakView()
-            }
-        }
+        model.node
     }
 }
 
